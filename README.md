53c9d0428574357ca9ffbae907dbfae984865437
John LaBarge <johnlabarge@google.com>
2017-09-01 15:42:27 -0500

    initial commit
    Change-Id: Iff027ad0ad7f0022cbe5892a04666aa96fdf33fe

---
+++ README.md
@@ -0,0 +1,28 @@
+# Customizing Fluentd for GKE Sample Code
+
+This is the sample code for the Customizing fluentd in Google Container Engine.  The tutorial explains how to run a custom fluentd daemonset inside of GKE that sends logs to StackDriver. 
+
+
+## Overview
+
+The repository contains 4 types of artifiacts
+1. Deployment manager scripts for creating a container-engine cluster with version 1.7.4 and cloud logging turned off in gke-cluster/
+2. kubernetes manifests for deploying a test logging program and the fluentd daemonset in kubernetes/
+3. The test logging program source and dockerfile in test-logger/
+4. Convenience scripts for speeding up some of the steps in the tutorial
+
+
+## Contact Us
+
+Please use [issue tracker](https://github.com/GoogleCloudPlatform/container-engine-customize-fluentd/issues)
+on GitHub to report any bugs, comments or questions regarding SDK development.
+
+We welcome all usage-related questions on [Stack Overflow](http://stackoverflow.com/questions/tagged/google-customizing-fluentd-in-container-engine)
+tagged with `google-cloud-container-engine`.
+
+## More Information
+
+* [Google Cloud Container Engine](https://cloud.google.com/container-engine/docs/)
+* [Google Cloud Deployment Manager](https://cloud.google.com/deployment-manager/docs/)
+* [Kubernetes](https://kubernetes.io/)
+* [Fluentd](https://docs.fluentd.org/v0.12/articles/quickstart)
---
+++ delete-cluster-service-account.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+# Copyright 2017 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+PROJECT=$(gcloud config get-value project)
+ACCOUNT_NAME="cluster-service-account"
+
+gcloud projects remove-iam-policy-binding $PROJECT \
+  --member serviceAccount:${ACCOUNT_NAME}@${PROJECT}.iam.gserviceaccount.com \
+  --role roles/logging.logWriter
+
+gcloud projects remove-iam-policy-binding $PROJECT \
+  --member serviceAccount:${ACCOUNT_NAME}@${PROJECT}.iam.gserviceaccount.com \
+  --role roles/monitoring.metricWriter
+  
+gcloud projects remove-iam-policy-binding $PROJECT \
+  --member serviceAccount:${ACCOUNT_NAME}@${PROJECT}.iam.gserviceaccount.com \
+  --role roles/iam.serviceAccountActor 
+  
+gcloud projects remove-iam-policy-binding $PROJECT \
+  --member serviceAccount:${ACCOUNT_NAME}@${PROJECT}.iam.gserviceaccount.com \
+  --role roles/container.admin 
+
+gcloud iam service-accounts delete ${ACCOUNT_NAME}@${PROJECT}.iam.gserviceaccount.com
